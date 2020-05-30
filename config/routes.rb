2c7fb83ebadd6153ae47e1e77a12a35c5056d1da
Takamie-Kai <alnico05@gmail.com>
2020-05-31 02:07:41 +0900

    create group controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   
+  # root "groups#index"
   root "messages#index"
   resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create]
+  resources :groups, only: [:new, :create, :edit, :update]
+
 end
